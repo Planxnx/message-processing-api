52f73c4c4971ff0e1181f144ef5d544daa8f08e3
Planxnx <60010465@kmitl.ac.th>
2020-09-05 01:30:21 +0700

    :beers: add mongodb connection to scheduler

--- scheduler-service/pkg/connection/connection.go
+++ scheduler-service/pkg/connection/connection.go
@@ -1,17 +1,28 @@
 package connection
 
 import (
+	"context"
 	"log"
+
+	"github.com/qiniu/qmgo"
 )
 
 type Connections struct {
-	MockCon *Mock
+	MockCon       *Mock
+	MongoDBClient *qmgo.Client
 }
 
 func InitializeConnection() *Connections {
 	log.Println("Initialize Conections")
+	ctx := context.Background()
+
+	mongodbclient, err := qmgo.NewClient(ctx, &qmgo.Config{Uri: "mongodb://admin:admin@localhost:27017"})
+	if err != nil {
+		panic(err)
+	}
 
 	return &Connections{
-		MockCon: InitializeMock(),
+		MockCon:       InitializeMock(),
+		MongoDBClient: mongodbclient,
 	}
 }
