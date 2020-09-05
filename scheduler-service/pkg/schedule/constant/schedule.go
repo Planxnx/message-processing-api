c29a87f7d60f132a054b525e5f4d8d4dd86bc980
Planxnx <60010465@kmitl.ac.th>
2020-09-05 23:57:52 +0700

    :ambulance: update scheduler methods

---
+++ scheduler-service/pkg/schedule/constant/schedule.go
@@ -0,0 +1,4 @@
+package constant
+
+const ScheduleType_DAILY string = "DAILY"
+const ScheduleType_EVERYHOUR string = "EVERY_HOUR"
--- scheduler-service/pkg/schedule/repository/schedule.go
+++ scheduler-service/pkg/schedule/repository/schedule.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/Planxnx/message-processing-api/scheduler-service/pkg/schedule/constant"
 	"github.com/Planxnx/message-processing-api/scheduler-service/pkg/schedule/model"
 	"github.com/qiniu/qmgo"
 	"go.mongodb.org/mongo-driver/bson"
@@ -25,11 +26,9 @@ func (schRepo *ScheduleRepository) GetDailySchedule(ctx context.Context) (*[]mod
 	hh, mm, _ := tNow.Clock()
 	workSchedule := &[]model.WorkSchedule{}
 	err := schRepo.WorkScheduleCollection.Find(ctx, bson.M{
-		"type": "DAILY",
-		"time": bson.M{
-			"hour":   hh,
-			"minute": mm,
-		},
+		"type":        constant.ScheduleType_DAILY,
+		"time.hour":   hh,
+		"time.minute": mm,
 	}).All(workSchedule)
 	if err != nil {
 		return nil, err
@@ -42,13 +41,52 @@ func (schRepo *ScheduleRepository) GetEveryHourSchedule(ctx context.Context) (*[
 	_, mm, _ := tNow.Clock()
 	workSchedule := &[]model.WorkSchedule{}
 	err := schRepo.WorkScheduleCollection.Find(ctx, bson.M{
-		"type": "EVERY_HOUR",
-		"time": bson.M{
-			"minute": mm,
-		},
+		"type":        constant.ScheduleType_EVERYHOUR,
+		"time.minute": mm,
 	}).All(workSchedule)
 	if err != nil {
 		return nil, err
 	}
 	return workSchedule, nil
+}
+
+func (schRepo *ScheduleRepository) GetAllSchedule(ctx context.Context) (*[]model.WorkSchedule, error) {
+	workSchedule := &[]model.WorkSchedule{}
+	err := schRepo.WorkScheduleCollection.Find(ctx, bson.M{}).All(workSchedule)
+	if err != nil {
+		return nil, err
+	}
+	return workSchedule, nil
+}
+
+func (schRepo *ScheduleRepository) GetAllDailySchedule(ctx context.Context) (*[]model.WorkSchedule, error) {
+	workSchedule := &[]model.WorkSchedule{}
+	err := schRepo.WorkScheduleCollection.Find(ctx, bson.M{
+		"type": constant.ScheduleType_DAILY,
+	}).All(workSchedule)
+	if err != nil {
+		return nil, err
+	}
+	return workSchedule, nil
+}
+
+func (schRepo *ScheduleRepository) GetAllEveryHourSchedule(ctx context.Context) (*[]model.WorkSchedule, error) {
+	workSchedule := &[]model.WorkSchedule{}
+	err := schRepo.WorkScheduleCollection.Find(ctx, bson.M{
+		"type": constant.ScheduleType_EVERYHOUR,
+	}).All(workSchedule)
+	if err != nil {
+		return nil, err
+	}
+	return workSchedule, nil
+}
+
+func (schRepo *ScheduleRepository) InsertDailySchedule(ctx context.Context, workSchedule model.WorkSchedule) (*qmgo.InsertOneResult, error) {
+	workSchedule.Type = constant.ScheduleType_DAILY
+	return schRepo.WorkScheduleCollection.InsertOne(ctx, workSchedule)
+}
+
+func (schRepo *ScheduleRepository) InsertEveryHourSchedule(ctx context.Context, workSchedule model.WorkSchedule) (*qmgo.InsertOneResult, error) {
+	workSchedule.Type = constant.ScheduleType_EVERYHOUR
+	return schRepo.WorkScheduleCollection.InsertOne(ctx, workSchedule)
 }
