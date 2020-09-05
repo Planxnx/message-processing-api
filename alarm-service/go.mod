03253e961a338892c8bb75cbbd8b276aedfebe2d
Planxnx <60010465@kmitl.ac.th>
2020-09-06 01:43:27 +0700

    :beers: remove none alarm pkg

--- alarm-service/go.mod
+++ alarm-service/go.mod
@@ -3,6 +3,8 @@ module github.com/Planxnx/message-processing-api/alarm-service
 go 1.14
 
 require (
+	github.com/Planxnx/message-processing-api/scheduler-service v0.0.0-20200905183250-875b708d112f
 	github.com/qiniu/qmgo v0.7.0
+	github.com/robfig/cron v1.2.0 // indirect
 	go.mongodb.org/mongo-driver v1.4.1
 )
--- alarm-service/main.go
+++ alarm-service/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Planxnx/message-processing-api/alarm-service/pkg/connection"
 
-	scheduleRepository "github.com/Planxnx/message-processing-api/alarm-service/pkg/schedule/repository"
-	scheduleUsecase "github.com/Planxnx/message-processing-api/alarm-service/pkg/schedule/usecase"
+	scheduleModel "github.com/Planxnx/message-processing-api/scheduler-service/pkg/schedule/model"
+	scheduleRepository "github.com/Planxnx/message-processing-api/scheduler-service/pkg/schedule/repository"
 )
 
 func main() {
+	ctx := context.Background()
 	connection := connection.InitializeConnection()
 
 	scheduleRepo := scheduleRepository.NewScheduleRepository(connection.MessageProcssingAPIDatabase.Collection("workSchedule"))
-	scheduleUsecase := scheduleUsecase.NewScheduleUsecase(scheduleRepo)
-	scheduleUsecase.CreateNewWorkSchedule()
+	result, err := scheduleRepo.InsertEveryHourSchedule(ctx, scheduleModel.WorkSchedule{
+		RefID:         "Test0000x",
+		Owner:         "Planx",
+		Message:       "แจ้งเตือนแสตนด์อัพมีทติ้ง",
+		CallbackTopic: "AlarmMessage",
+		Time: scheduleModel.WorkTime{
+			Date:   "2020-03-17",
+			Hour:   10,
+			Minute: 30,
+			Second: 30,
+		},
+	})
+	if err != nil {
+		log.Printf("Error: failed on insert: %s", err.Error())
+		return
+	}
+	log.Printf("Insert successful: %v", result.InsertedID)
 	log.Println("Start Alarm-service...")
 }
