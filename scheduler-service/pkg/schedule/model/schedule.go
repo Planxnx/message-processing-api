8d55bd42b8833b63b5efa59ae0fa8204e40e89f3
Planxnx <60010465@kmitl.ac.th>
2020-09-12 04:49:08 +0700

    :beers: update work schedule

--- scheduler-service/pkg/schedule/model/schedule.go
+++ scheduler-service/pkg/schedule/model/schedule.go
@@ -3,23 +3,25 @@ package model
 import "time"
 
 type WorkSchedule struct {
-	Ref1          string    `bson:"ref1"`  //client reference
-	Ref2          string    `bson:"ref2"`  //message reference
-	Ref3          string    `bson:"ref3"`  //end-user reference
-	Owner         string    `bson:"owner"` //service reference
-	CallbackTopic string    `bson:"callbackTopic"`
-	Message       string    `bson:"message"`
-	Time          WorkTime  `bson:"time"`
-	Type          string    `bson:"type"` //Daily, Hourly
-	CreateAt      time.Time `bson:"createdAt"`
-	DeletedAt     time.Time `bson:"deletedAt"`
+	Ref1          string                 `bson:"ref1" json:"ref1"`   //client reference
+	Ref2          string                 `bson:"ref2" json:"ref2"`   //message reference
+	Ref3          string                 `bson:"ref3" json:"ref3"`   //end-user reference
+	Owner         string                 `bson:"owner" json:"owner"` //service reference
+	CallbackTopic []string               `bson:"callbackTopic" json:"callbackTopic"`
+	Message       string                 `bson:"message" json:"message"`
+	Data          map[string]interface{} `bson:"data" json:"data"` //attachment
+	Time          WorkTime               `bson:"time" json:"time"`
+	Type          string                 `bson:"type" json:"type"` //Daily, Hourly
+	CreateAt      time.Time              `bson:"createdAt" json:"createdAt"`
+	DeletedAt     time.Time              `bson:"deletedAt" json:"deletedAt"`
+	Features      map[string]bool        `bson:"features" json:"features"` //Feature this message will uses next
 }
 
 type WorkTime struct {
-	Timestamp time.Time `bson:"timestamp"` //ISO date
-	Day       int       `bson:"day"`
-	WeekDay   string    `bson:"weekDay"`
-	Hour      int       `bson:"hour"`
-	Minute    int       `bson:"minute"`
-	Second    int       `bson:"second"`
+	Timestamp time.Time `bson:"timestamp" json:"ref1"` //ISO date
+	Day       int       `bson:"day" json:"ref1"`
+	WeekDay   string    `bson:"weekDay" json:"ref1"`
+	Hour      int       `bson:"hour" json:"ref1"`
+	Minute    int       `bson:"minute" json:"ref1"`
+	Second    int       `bson:"second" json:"ref1"`
 }
