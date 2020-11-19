1de35d65a9607bd65129ec3c74ee760f7d9fe9b4
Planxnx <60010465@kmitl.ac.th>
2020-11-19 20:52:23 +0700

    :tada: example line with chitchat

--- example-provider-service/example-line/src/routes/webhook.js
+++ example-provider-service/example-line/src/routes/webhook.js
@@ -1,8 +1,45 @@
+import * as line from '@line/bot-sdk';
+
 import { Router } from 'express';
+import config from '../config';
+import mpaService from '../services/message-processing-api';
+
 const router = Router();
 
+//lazy cache
+let userRefReplyToken = {};
+
+const lineClient = new line.Client({
+  channelAccessToken: config.line.channelAccessToken,
+});
+
 router.post('/message-processing-api', async (req, res, next) => {
-  console.log(req.body)
+  console.log(`Received MPA webhook events:${JSON.stringify(req.body)}\n`);
+  let replyToken = userRefReplyToken[req.body.ref3];
+  try {
+    await lineClient.replyMessage(replyToken, {
+      type: 'text',
+      text: req.body.data.message,
+    });
+  } catch (error) {
+    await lineClient.pushMessage(req.body.ref3, {
+      type: 'text',
+      text: req.body.data.message,
+    });
+  }
+  res.json({});
+});
+
+router.post('/line', async (req, res, next) => {
+  const events = req.body.events;
+  console.log(`Received Line webhook events:${JSON.stringify(events)}\n`);
+  for (let event of events) {
+    if (event.type == 'message') {
+      let userRef = event.source.userId;
+      userRefReplyToken[userRef] = event.replyToken;
+      mpaService.sendChitchat(event.message.text, userRef);
+    }
+  }
   res.json({});
 });
 
