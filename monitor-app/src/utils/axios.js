e0828658904f8f2d5d489c3d0e798efa16d2432c
Planxnx <60010465@kmitl.ac.th>
2021-06-04 02:18:19 +0700

    :boom: update pkg

--- monitor-app/src/utils/axios.js
+++ monitor-app/src/utils/axios.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    process.env.REACT_APP_API_URL ||
-    "https://26e6ccc2a0a8.ngrok.io" ||
-    "http://localhost:8080",
+    process.env.REACT_APP_API_URL || "http://localhost:8080",
   timeout: 1000 * 15,
 });
 
